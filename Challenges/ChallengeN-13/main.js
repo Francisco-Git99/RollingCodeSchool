b06aad3d56a27941309da9a49f0391d56d080a64
Francisco! <69412832+Francisco-Github99@users.noreply.github.com>
2023-10-20 15:54:44 -0300

    Add files via upload

---
+++ Challenges/ChallengeN-13/main.js
@@ -0,0 +1,27 @@
+// --- Punto 1 ---
+let saludo1 = "hola";
+let saludo2 = "gente";
+
+document.write(`${saludo1} ${saludo2}`);
+document.write("<br>");
+document.write(saludo1 +" "+ saludo2);
+
+// --- Punto 2 ----
+let numero1 = 20;
+let numero2 = 10;
+
+console.log("Suma: de 20 + 10 = ", numero1+numero2);
+console.log("Resta: de 20 - 10 = ", numero1-numero2);
+console.log("Producto: de 20 * 10 = ", numero1*numero2);
+console.log("Division: de 20/10 = ", numero1/numero2);
+console.log("El resto da "+ numero1%numero2 +" , es par");
+console.log("<br>");
+
+// ---- Punto 3 -----
+let numero3 = 348;
+
+console.log("Suma: de 20 + 10 + la tercera variable = ", numero1+numero2+numero3);
+console.log("Producto: de 20 * 10 * la tercera varible = ", numero1*numero2*numero3);
+console.log("Resta: de 20 - 10 - la tercera variable = ", numero1-numero2-numero3);
+
+
